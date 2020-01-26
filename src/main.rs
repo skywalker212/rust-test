e62132d80af5efdb5c52660e788275176fa9b467
Akash <akashgajjar8@gmail.com>
2020-01-26 13:10:44 +0530

    initiate genesis coding procedure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rltk::{Rltk, GameState, Console};
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Houston");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let context = Rltk::init_simple8x8(80, 50, "TARS", "resources");
+    let gs =  State{};
+    rltk::main_loop(context, gs);
 }
